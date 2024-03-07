01a2df9257c4f9ab53b456c01c9e44db4776a8f0
mastablasta1 <93185543+mastablasta1@users.noreply.github.com>
2024-03-07 10:44:32 +0300

    Create chpu.php

---
+++ chpu.php
@@ -0,0 +1,33 @@
+<?
+    /*
+        Функции для работы с ЧПУ
+        !!!Уазать максимальную длину итоговой строки в переменной $size 
+    */
+        
+    function chpu($value)
+    {
+        
+        $size = 200;
+
+        $converter = array(
+            'а' => 'a',    'б' => 'b',    'в' => 'v',    'г' => 'g',    'д' => 'd',
+            'е' => 'e',    'ё' => 'e',    'ж' => 'zh',   'з' => 'z',    'и' => 'i',
+            'й' => 'y',    'к' => 'k',    'л' => 'l',    'м' => 'm',    'н' => 'n',
+            'о' => 'o',    'п' => 'p',    'р' => 'r',    'с' => 's',    'т' => 't',
+            'у' => 'u',    'ф' => 'f',    'х' => 'h',    'ц' => 'c',    'ч' => 'ch',
+            'ш' => 'sh',   'щ' => 'sch',  'ь' => '',     'ы' => 'y',    'ъ' => '',
+            'э' => 'e',    'ю' => 'yu',   'я' => 'ya',
+        );
+    
+        $value = mb_strtolower($value);
+        $value = strtr($value, $converter);
+        $value = mb_ereg_replace('[^-0-9a-z]', '-', $value);
+        $value = mb_ereg_replace('[-]+', '-', $value);
+        $value = trim($value, '-');	
+
+        $value = substr($value, 0, $size);
+    
+        return $value;
+    }    
+
+?>
